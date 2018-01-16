b2c413293bf96869849006fc3a1135c5dac2bfa3
yuhan <guoyuhan819@gmail.com>
2018-01-16 21:00:38 +0100

    update tatoeba

--- enfr/scripts/tatoeba_clean.py
+++ enfr/scripts/tatoeba_clean.py
@@ -3,14 +3,16 @@ import argparse
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', help='data directory', 
+parser.add_argument('--input_dir', help='input directory', 
+                    type=str, required=True)
+parser.add_argument('--output_dir', help='output directory', 
                     type=str, required=True)
 args = parser.parse_args()
 
 # load data
-dfBase = pd.read_csv(os.path.join(args.data_dir, 'sentences.csv'), sep='\t', header=None, names=['label', 'lang', 'text'])
+dfBase = pd.read_csv(os.path.join(args.input_dir, 'sentences.csv'), sep='\t', header=None, names=['label', 'lang', 'text'])
 dfBase = dfBase.set_index('label')
-dfLink = pd.read_csv(os.path.join(args.data_dir, 'links.csv'), sep='\t', header=None, names=['ida', 'idb'])
+dfLink = pd.read_csv(os.path.join(args.input_dir, 'links.csv'), sep='\t', header=None, names=['ida', 'idb'])
 
 # lang label
 label_en = set(dfBase[dfBase['lang']=='eng'].index.tolist())
@@ -28,8 +30,8 @@ for r in dfLink.itertuples():
 lang_enfr = list(lang_pair)
 
 # save data
-f_en = open(os.path.join(args.data_dir, 'tatoeba.en'), 'w')
-f_fr = open(os.path.join(args.data_dir, 'tatoeba.fr'), 'w')
+f_en = open(os.path.join(args.output_dir, 'tatoeba.en'), 'w')
+f_fr = open(os.path.join(args.output_dir, 'tatoeba.fr'), 'w')
 
 for t in lang_enfr:
     f_en.write(dfBase.loc[t[0],'text'].strip())
